5430e8be864f03722084515e9a08b7b9cdc42bde
Vorex <esilva.romain@gmail.com>
2013-11-02 19:55:52 +0100

    Add init method that convert the map to json.

--- assets/js/lib/Mars/common/map.js
+++ assets/js/lib/Mars/common/map.js
@@ -5,10 +5,16 @@
 	 * [ description]
 	 * @return {[type]} [description]
 	 */
-	nsCommon.Map = function(map) {
+	nsCommon.Map = function(terrain) {
 		/* Define attributes. */
-		this.terrain = {};
-		this.squares = [];
+		this._terrain = terrain;
+		this._squares = [];
+
+		this._init(this._terrain);
+	};
+
+	nsCommon.Map.prototype._init = function() {
+		this._fromJSON();
 	};
 
 	/**
@@ -16,6 +22,6 @@
 	 * @return {[type]} [description]
 	 */
 	nsCommon.Map.prototype._fromJSON = function() {
-
+		JSON.parse(this._terrain);
 	};
 })();
