817e658a55bb2d4b49d259199b51f322138beca8
Vorex <esilva.romain@gmail.com>
2013-11-03 11:39:28 +0100

    Add test function to map class for testing purpose.

--- assets/js/lib/Mars/common/map.js
+++ assets/js/lib/Mars/common/map.js
@@ -7,21 +7,29 @@
 	 */
 	nsCommon.Map = function(terrain) {
 		/* Define attributes. */
-		this._terrain = terrain;
+		this._terrain = {};
 		this._squares = [];
 
-		this._init(this._terrain);
+		this._init(terrain);
 	};
 
-	nsCommon.Map.prototype._init = function() {
-		this._fromJSON();
+	nsCommon.Map.prototype._init = function(terrain) {
+		this._fromJSON(terrain);
 	};
 
 	/**
 	 * [ description]
 	 * @return {[type]} [description]
 	 */
-	nsCommon.Map.prototype._fromJSON = function() {
-		JSON.parse(this._terrain);
+	nsCommon.Map.prototype._fromJSON = function(terrain) {
+		this._terrain = JSON.parse(terrain);
+	};
+
+	nsCommon.Map.prototype.getWidth = function() {
+		return 10;
+	};
+
+	nsCommon.Map.prototype.getHeight = function() {
+		return 10;
 	};
 })();
