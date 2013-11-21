362d638cc73986ad2b2064784cf41b75f42e1ce4
vdaguenet <valentin.daguenet@gmail.com>
2013-11-21 15:43:05 +0100

    Add iron material

---
+++ assets/js/lib/Mars/common/materials/iron.js
@@ -0,0 +1,22 @@
+(function() {
+	var nsMaterials = using('mars.common.material');
+
+	/**
+	 * Class Iron
+	 * Represent the material iron with its color and its id in JSon file.
+	 */
+	nsMaterials.Iron = function() {
+		this.id = 4;
+		this.name = 'Iron';
+		this.textureColor = "#C9A78E";
+		this.texture = 'iron.jpg';
+
+		/* If already instancied, return instance (Singleton) */
+		if (arguments.callee.instance) {
+			return arguments.callee.instance;
+		}
+		else {
+			arguments.callee.instance = this;
+		}
+	};
+})();
