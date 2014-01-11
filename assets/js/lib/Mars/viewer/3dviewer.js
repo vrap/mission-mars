422f7b8b58e3b0eb81ba7aa82892087c64c1dfbf
Aurelien <aureliendumont@MacBook-Pro.local>
2014-01-11 12:09:11 +0100

    add round mini map

--- assets/js/lib/Mars/viewer/3dviewer.js
+++ assets/js/lib/Mars/viewer/3dviewer.js
@@ -105,6 +105,39 @@
 				index++;
 			}
 		}
+
+		var urls = [
+		  './assets/img/sky/4.jpg',
+		  './assets/img/sky/2.jpg',
+		  './assets/img/sky/6.jpg',
+		  './assets/img/sky/5.jpg',
+		  './assets/img/sky/1.jpg',
+		  './assets/img/sky/3.jpg'
+		];
+
+		var cubemap = THREE.ImageUtils.loadTextureCube(urls); // load textures
+		cubemap.format = THREE.RGBFormat;
+
+		var shader = THREE.ShaderLib['cube']; // init cube shader from built-in lib
+		shader.uniforms['tCube'].value = cubemap; // apply textures to shader
+
+		// create shader material
+		var skyBoxMaterial = new THREE.ShaderMaterial( {
+		  fragmentShader: shader.fragmentShader,
+		  vertexShader: shader.vertexShader,
+		  uniforms: shader.uniforms,
+		  depthWrite: false,
+		  side: THREE.BackSide
+		});
+
+		// create skybox mesh
+		var skybox = new THREE.Mesh(
+		  new THREE.CubeGeometry(1000, 1000, 1000),
+		  skyBoxMaterial
+		);
+
+		this.scene.add(skybox);
+
 	}
 
 
