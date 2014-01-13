d5e0335b86c24b7a32078602f11a81763fd09634
vdaguenet <valentin.daguenet@gmail.com>
2014-01-13 11:15:58 +0100

    Begining of home page

--- assets/js/interface.js
+++ assets/js/interface.js
@@ -1,6 +1,22 @@
 /**
 * File for functions used for UI and UX
 */
+var scripts = [
+		// Main script
+		"assets/js/main.js"
+];
+
+document.body.onload = function () {
+	document.querySelector('.home').style.display = 'block';
+	document.querySelector('.view').style.display = 'none';
+}
+
+document.querySelector('#show').onclick = function () {
+	createScripts(scripts);
+	document.querySelector('.home').style.display = 'none';
+	document.querySelector('.view').style.display = 'block';
+}
+
 /* Control */
 document.querySelector('#controls-header').onclick = function () {
 	if (document.querySelector('#panel-control').className == 'closed') {
@@ -11,6 +27,7 @@ document.querySelector('#controls-header').onclick = function () {
 		document.querySelector('#panel-control').className = 'closed';
 	}
 }
+
 /* Console */
 var askConcoleElement = document.querySelector('#form-commande');
 var responseConsoleElement = document.querySelector('#panel-response-console');
@@ -20,5 +37,13 @@ document.querySelector('#form-commande').onsubmit = function(e){
 		e.preventDefault();
 		var commande = this.elements["commande"];
 		consoleInvader.getCommande(commande);
+}
 
+function createScripts (scriptsArray) {
+	for (var i = 0; i < scriptsArray.length; i++) {
+		var s = document.createElement('script');
+		s.src = scriptsArray[i];
+
+		document.body.appendChild(s);
+	};
 }
