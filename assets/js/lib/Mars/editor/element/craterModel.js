71ed3805f3be44faf46dcc6cffac6478d5ebb935
vdaguenet <valentin.daguenet@gmail.com>
2014-01-16 14:04:21 +0100

    Cleaning code. Add commentaries

--- assets/js/lib/Mars/editor/element/craterModel.js
+++ assets/js/lib/Mars/editor/element/craterModel.js
@@ -2,10 +2,7 @@
 	var nsElements = using('mars.editor.element');
 
 	/**
-	 * [ description]
-	 *
-	 * @constructor
-	 * @this {CraterModel}
+	 * Constructor
 	 */
 	nsElements.CraterModel = function() {
 		/* Inherit from parent, and call his constructor. */
@@ -15,42 +12,36 @@
 
 	/**
 	 * Inherit from Model class.
-	 * 
-	 * @this {CraterModel}
 	 */
 	nsElements.CraterModel.prototype.__proto__ = nsElements.Model.prototype;
 
 	/**
-	 * [ description]
+	 * Create a crater
 	 *
-	 * @this {CraterModel}
-	 * @param  {int} width     [description]
-	 * @param  {int} height    [description]
-	 * @param  {array} materials [description]
+	 * @param  {int} width
+	 * @param  {int} height
+	 * @param  {array} materials Materials availables for this element
 	 */
 	nsElements.CraterModel.prototype.create = function(width, height, materials) {
 		/* Initialize the creation of the element. */
 		this.parent.create.call(this, width, height, materials);
 
-		var minSize = (width <= height) ? width : height;
-		var z = getRandomInt(2, Math.round(this._zMax/2));
-
-		var x_center = Math.round(width/2)-1,
+		var minSize = (width <= height) ? width : height,
+			z = getRandomInt(2, Math.round(this._zMax/2)),
+			x_center = Math.round(width/2)-1,
 			y_center = Math.round(height/2)-1,
 			rayon =	Math.round(minSize/2)-1;
 
+		// Up Z value from the outside to the middle of the crater
 		while (rayon >= 1) {
-
 			drawCircle (x_center, y_center, rayon, z, this._element);
-			
+			// Reduce Z only if it's grater than  zMin to don't have a cone
 			if (z >= this._zMin) {
 				var z_temp = 1;
 				z-=z_temp;
 			}
-
 			rayon--;
 		}
-
 		/* Return the element and clean the var. */
 		return this._toJSON(true);
 	};
--- assets/js/lib/Mars/editor/element/hillModel.js
+++ assets/js/lib/Mars/editor/element/hillModel.js
@@ -21,36 +21,32 @@
 	nsElements.HillModel.prototype.__proto__ = nsElements.Model.prototype;
 
 	/**
-	 * [ description]
+	 * Create an hill
 	 *
 	 * @this {HillModel}
-	 * @param  {int} width     [description]
-	 * @param  {int} height    [description]
-	 * @param  {array} materials [description]
+	 * @param  {int} width
+	 * @param  {int} height
+	 * @param  {array} materials Allowed materials for this element
 	 */
 	nsElements.HillModel.prototype.create = function(width, height, materials) {
+		// Create mini map from parent
 		this.parent.create(width, height, materials);
 
-		var minSize = (width <= height) ? width : height;
-		var z = getRandomInt(1, Math.round(this._zMax/10));
-
-		var x_center = Math.round(width/2)-1,
+		var minSize = (width <= height) ? width : height,
+			z = getRandomInt(1, Math.round(this._zMax/10)),
+			x_center = Math.round(width/2)-1,
 			y_center = Math.round(height/2)-1,
 			rayon =	Math.round(minSize/2)-1;
 
 		while (rayon >= 1) {
-
 			drawCircle (x_center, y_center, rayon, z, this._element);
-			
+			// Increase Z value			
 			if (z <= this._zMax) {
 				var z_temp = getRandomInt(1, 2);
 				z+=z_temp;
 			}
-
 			rayon--;
 		}
-
-
 		/* Return the element and clean the var. */
 		return this._toJSON(true);
 	};
