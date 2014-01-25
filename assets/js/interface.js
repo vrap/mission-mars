7ab92bf0b1eb93381e7b1f8b30b269515e433f6c
vdaguenet <valentin.daguenet@gmail.com>
2014-01-25 16:00:47 +0100

    Implement form verification

--- assets/js/interface.js
+++ assets/js/interface.js
@@ -4,16 +4,21 @@
 
 (function () {
 	/**
-	 * Display home page and hide viewer
+	 * Home page gestion
 	 */
 	document.querySelector('.view').style.display = 'none';
   document.querySelector('#voyager-settings').style.display = 'none';
   document.querySelector('#explorer-settings').style.display = 'none';
 
+  /**
+   * First panel
+   */
   document.querySelector('#start').onclick = function () {
     document.querySelector('#st-control-2').checked = true;
   };
-
+  /**
+   * Second panel
+   */
   document.querySelector('#import').onclick = function () {
     document.querySelector('#st-control-3').checked = true;
     document.querySelector('#block-generate').style.display = 'none';
@@ -25,25 +30,94 @@
     document.querySelector('#block-generate').style.display = 'block';
     document.querySelector('#block-upload').style.display = 'none';
   };
-
+  /**
+   * Third panel
+   */
   document.querySelector('#scenario').onclick = function () {
+    if(isNaN(document.querySelector('#map-size').value) || document.querySelector('#map-size').value < 1) {
+      alert("Invalid map size !");
+
+      return false;
+    }
     document.querySelector('#st-control-4').checked = true;
   };
 
+  /**
+   * Fourth panel
+   */
   document.querySelector('#explorer').onclick = function () {
     document.querySelector('#voyager-settings').style.display = 'none';
-    document.querySelector('#explorer-settings').style.display = 'inline-block';
+    document.querySelector('#explorer-settings').style.display = 'block';
   };
 
   document.querySelector('#voyager').onclick = function () {
-    document.querySelector('#voyager-settings').style.display = 'inline-block';
+    document.querySelector('#voyager-settings').style.display = 'block';
     document.querySelector('#explorer-settings').style.display = 'none';
   };
 
+  /**
+   * Viewer
+   */
   document.querySelector('#view').onclick = function () {
-    createScript("assets/js/main.js");
-    document.querySelector('.st-container').style.display = 'none';
-    document.querySelector('.view').style.display = 'block';
+    if(document.querySelector('#explorer').checked) {
+      // Explorer settings verifications
+      if(!isPositiveInteger(document.querySelector('#explorer-energy').value)
+          || parseInt(document.querySelector('#explorer-energy').value) < 1) {
+        alert("Invalid energy value !");
+
+        return false;
+      } else if(!isPositiveInteger(document.querySelector('#explorer-startX').value)
+          || parseInt(document.querySelector('#explorer-startX').value) > parseInt(document.querySelector('#map-size').value)) {
+        alert("Invalid starting position X !");
+
+        return false;
+      } else if(!isPositiveInteger(document.querySelector('#explorer-startY').value)
+          || parseInt(document.querySelector('#explorer-startY').value) > parseInt(document.querySelector('#map-size').value)) {
+        alert("Invalid starting position Y !");
+
+        return false;
+      } else {
+        createScript("assets/js/main.js");
+        document.querySelector('.st-container').style.display = 'none';
+        document.querySelector('.view').style.display = 'block';
+      }
+    } else if (document.querySelector('#voyager').checked) {
+      // Voyager settings verifications
+      if(!isPositiveInteger(document.querySelector('#voyager-energy').value)
+          || parseInt(document.querySelector('#voyager-energy').value) < 1) {
+        alert("Invalid energy value !");
+
+        return false;
+      } else if(!isPositiveInteger(document.querySelector('#voyager-startX').value)
+          || parseInt(document.querySelector('#voyager-startX').value) > parseInt(document.querySelector('#map-size').value)) {
+        alert("Invalid starting position X !");
+
+        return false;
+      } else if(!isPositiveInteger(document.querySelector('#voyager-startY').value)
+          || parseInt(document.querySelector('#voyager-startY').value) > parseInt(document.querySelector('#map-size').value)) {
+        alert("Invalid starting position Y !");
+
+        return false;
+      } else if(!isPositiveInteger(document.querySelector('#voyager-endX').value)
+          || parseInt(document.querySelector('#voyager-endX').value) > parseInt(document.querySelector('#map-size').value)) {
+        alert("Invalid ending position X !");
+
+        return false;
+      } else if(!isPositiveInteger(document.querySelector('#voyager-endY').value)
+          || parseInt(document.querySelector('#voyager-endY').value) > parseInt(document.querySelector('#map-size').value)) {
+        alert("Invalid ending position Y !");
+
+        return false;
+      } else {
+        createScript("assets/js/main.js");
+        document.querySelector('.st-container').style.display = 'none';
+        document.querySelector('.view').style.display = 'block';
+      }
+    } else {
+      alert('Select a scenario.');
+
+      return false;
+    }
   };
 
 	/**
@@ -91,17 +165,8 @@
 			reader.readAsText(file);
       return true;
 		} else {
-      alert('Please, upload a JSon');
+      alert('Please upload a JSon.');
       return false;
     }
 	}
 })();
-
-/**
- * Add scripts at the end of the body
- */
-function createScript (script) {
-  var s = document.createElement('script');
-  s.src = scriptsArray[i];
-  document.body.appendChild(s);
-}
