ab2b7fdcbedb4ec595d3211d81293199255d417c
Romain <r.esilva@sysdream.com>
2014-01-29 01:58:59 +0100

    Add a function to make some loops with deferreds.

--- assets/js/helper/utils.js
+++ assets/js/helper/utils.js
@@ -127,3 +127,28 @@ function createScript (script) {
 function isPositiveInteger(n) {
   return (!isNaN(n) && parseInt(n) >= 0);
 }
+
+// `condition` is a function that returns a boolean
+// `body` is a function that returns a promise
+// returns a promise for the completion of the loop
+function promiseWhile(condition, body) {
+    var done = Q.defer();
+
+    function loop() {
+        // When the result of calling `condition` is no longer true, we are
+        // done.
+        if (!condition()) return done.resolve();
+        // Use `when`, in case `body` does not return a promise.
+        // When it completes loop again otherwise, if it fails, reject the
+        // done promise
+        Q.when(body(), loop, done.reject);
+    }
+
+    // Start running the loop in the next tick so that this function is
+    // completely async. It would be unexpected if `body` was called
+    // synchronously the first time.
+    Q.nextTick(loop);
+
+    // The promise
+    return done.promise;
+}
