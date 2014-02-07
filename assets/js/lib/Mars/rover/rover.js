704eac48ff6ea1d93c09ddd7e800123a1175309c
Romain <r.esilva@sysdream.com>
2014-02-07 20:20:18 +0100

    Fix slope calcul

--- assets/js/lib/Mars/rover/rover.js
+++ assets/js/lib/Mars/rover/rover.js
@@ -293,7 +293,14 @@
 	    distance = 1;
 	}
 
-	return Math.abs(Math.round((destinationZ - currentZ) / (distance *5)));
+	distance = distance * 5;
+	current = currentZ * 5;
+	destination = destinationZ * 5;
+
+	var slope = (destination - current) / distance;
+	slope = Math.round(Math.abs(slope));
+
+	return slope;
     };
 
     /**
@@ -395,9 +402,9 @@
 
 			    /* Calculate the cost of travel and removes from tank. */
 			    if (moveCost <= this.tank) {
-				var slope = this.calculateSlop(lastZ, destinationSquare.z, 1 * 5);
+				var slope = this.calculateSlop(lastZ, destinationSquare.z, 1);
 
-				if (slope >= 4) {
+				if (slope <= 4) {
 				    // The distance cost plus the tank cost from the elevation
 				    var elevationCost = moveCost * (1 + slope);
 				    var finalCost = elevationCost + moveCost;
