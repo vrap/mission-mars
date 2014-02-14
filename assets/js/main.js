acadc552c1974854ff98ce7d6c4afc1d890503f3
Romain <r.esilva@sysdream.com>
2014-02-14 22:06:10 +0100

    Refactoring of main script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,186 +1,204 @@
 (function() {
-	/* Loading namespaces. */
-	var nsCommon = using('mars.common');
-	var nsViewer = using('mars.viewer');
-	var nsEditor = using('mars.editor');
-	var nsRover = using('mars.rover');
-	var nsSpeculator = using('mars.rover.speculator3000');
-	var nsMaterial = using('mars.common.material');
-	var nsElements = using('mars.editor.element');
+    /* Loading namespaces. */
+    var nsCommon = using('mars.common');
+    var nsViewer = using('mars.viewer');
+    var nsEditor = using('mars.editor');
+    var nsRover = using('mars.rover');
+    var nsSpeculator = using('mars.rover.speculator3000');
+    var nsMaterial = using('mars.common.material');
+    var nsElements = using('mars.editor.element');
 
-	/* Define viewer container. */
-	var renderDiv = document.querySelector('#render');
-	var render2dDiv = document.querySelector('#minimap');
+    /* Loading observer. */
+    var observable = new nsCommon.Observable();
 
-	var renderFull2dDiv = document.querySelector('#fullmap');
+    /* Initializing map related var. */
+    var terrain, map, mapSize;
 
-	/* Generate a map. */
-	var map,
-			terrain;
-	if( "" != document.getElementById('json').value) {
-		// When a JSon is uploaded
-		terrain = document.getElementById('json').value;
-		map = new nsCommon.Map(terrain);
-	} else {
-		// Map generation otherwise.
-    var mapSize = document.querySelector('#map-size').value;
+    /* If user upload map load it; otherwize generate it. */
+    if (document.getElementById('json').value != '') {
+	terrain = document.getElementById('json').value;
+	map = new nsCommon.Map(terrain);
+    }
+    else {
+	/* Retrieve defined map size. */
+	mapSize = document.querySelector('#map-size').value;
 
-    /* Loading materials. */
-    var materialRock = new nsMaterial.Rock();
-    var materialIce = new nsMaterial.Ice();
-    var materialIron = new nsMaterial.Iron();
-    var materialOre = new nsMaterial.Ore();
-    var materialSand = new nsMaterial.Sand();
-    var materialOther = new nsMaterial.Other();
+	/* Loading materials. */
+	var materialRock  = new nsMaterial.Rock();
+	var materialIce   = new nsMaterial.Ice();
+	var materialIron  = new nsMaterial.Iron();
+	var materialOre   = new nsMaterial.Ore();
+	var materialSand  = new nsMaterial.Sand();
+	var materialOther = new nsMaterial.Other();
 
-    /* Loading elements. */
-    var elementCrater = new nsElements.CraterModel([materialSand], -40, 100, 2);
-    var elementHill   = new nsElements.HillModel([materialSand], 20, 200, 2);
-    var elementRavine = new nsElements.RavineModel([materialRock], -60, 20, 2);
+	/* Loading elements model. */
+	var elementCrater = new nsElements.CraterModel([materialSand], -40, 100, 2);
+	var elementHill   = new nsElements.HillModel([materialSand], 20, 200, 2);
+	var elementRavine = new nsElements.RavineModel([materialRock], -60, 20, 2);
 
-		terrain = nsEditor.TerrainGenerator.generate([materialRock, materialIce, materialIron, materialOre, materialSand, materialOther], 
-			[{
-				"model": elementCrater,
-				"number": Math.floor((mapSize / 10) * 1.5)
-			},
-			{
-				"model": elementHill,
-				"number": Math.floor(mapSize / 25)
-			},
-			{
-				"model": elementRavine,
-				"number": Math.floor((mapSize / 25) * 0.3)
-			}],
-			mapSize, mapSize, -5, 5);
-		map = new nsCommon.Map(terrain);
+	/* Generate a terrain. */
+	terrain = nsEditor.TerrainGenerator.generate(
+	    [
+		materialRock,
+		materialIce,
+		materialIron,
+		materialOre,
+		materialSand,
+		materialOther
+	    ],
+	    [
+		{
+		    "model": elementCrater,
+		    "number": Math.floor((mapSize / 10) * 1.5)
+		},
+		{
+		    "model": elementHill,
+		    "number": Math.floor(mapSize / 25)
+		},
+		{
+		    "model": elementRavine,
+		    "number": Math.floor((mapSize / 25) * 0.3)
+		}
+	    ],
+	    mapSize,
+	    mapSize,
+            -10,
+	    10
+	);
+
+	/* Load map from the generated terrain. */
+	map = new nsCommon.Map(terrain);
+    }
+
+    /* Define viewers container. */
+    var renderDiv = document.querySelector('#render');
+    var render2dDiv = document.querySelector('#minimap');
+    var renderFull2dDiv = document.querySelector('#fullmap');
+    
+    /* Init viewer manager. */
+    var viewer     = new nsViewer.Viewer(map);
+    var viewerFull = new nsViewer.Viewer(map);
+
+    /* Load 2D viewer (radar). */
+    viewer.load2D(render2dDiv, '', true);
+
+    /* Load map in full screen 2D viewer. */
+    viewerFull.load2D(renderFull2dDiv, '', false);
+
+    /* Load 3D viewer. */
+    viewer.load3D(renderDiv, {fog: 0.002, wireframe: false, axis: true});
+
+    /* Load infos of robot */
+    var elementBattery = document.querySelector('#battery'),
+    elementPosition = document.querySelector('#position'),
+    elementMaterial = document.querySelector('#material'),
+    elementMove = document.querySelector('#move'),
+    elementMiniMap = document.querySelector('#bloc-panel-minimap-hover'),
+    elementControl = document.querySelector('#display-control'),
+    wireframeOn = document.querySelector('#w1'),
+    wireframeOff = document.querySelector('#w2'),
+    moreFog = document.querySelector('#more'),
+    lessFog = document.querySelector('#less'),
+    cameraOn = document.querySelector('#c1'),
+    cameraOff = document.querySelector('#c2'),
+    downloadMap = document.querySelector('#download'),
+    blocPop = document.querySelector('#bloc-pop');
+
+    var elements = {
+	'elementBattery': elementBattery,
+	'elementPosition': elementPosition,
+	'elementMaterial': elementMaterial,
+	'elementMove': elementMove,
+	'elementMiniMap': elementMiniMap,
+	'elementControl': elementControl,
+	'control' : {
+	    'wireframeOn': wireframeOn,
+	    'wireframeOff': wireframeOff,
+	    'moreFog': moreFog,
+	    'lessFog': lessFog,
+	    'cameraOn': cameraOn,
+	    'cameraOff': cameraOff,
+	    'downloadMap': downloadMap,
+	    'terrain': [terrain]
+	},
+	'pop' : {
+	    'blocPop': blocPop,
+	    'classPop': 'pop'
 	}
-	
-	/* Load map in viewer. */
-  // Init viewer
-	var viewer = new nsViewer.Viewer(map);
-  // Load map in 2D
-	viewer.load2D(render2dDiv,'',true);
-  // Load map in full screen 2D viewer
-	var viewerFul = new nsViewer.Viewer(map);
-	viewerFul.load2D(renderFull2dDiv, '' ,false);
-  // Load map in 3D
-	viewer.load3D(renderDiv, {fog: 0.002, wireframe: false, axis: true});
+    }
+    
+    var elementViewer = viewer.viewers[renderDiv],
+    interfaces = new nsViewer.Interface(elemements, elemementViewer);
 
-	/* Load infos of robot */
-	var elementBattery = document.querySelector('#battery'),
-	 elementPosition = document.querySelector('#position'),
-	 elementMaterial = document.querySelector('#material'),
-	 elementMove = document.querySelector('#move'),
-	 elementMiniMap = document.querySelector('#bloc-panel-minimap-hover'),
-	 elementControl = document.querySelector('#display-control'),
-	 wireframeOn = document.querySelector('#w1'),
-   	wireframeOff = document.querySelector('#w2'),
-	 moreFog = document.querySelector('#more'),
-	 lessFog = document.querySelector('#less'),
-	 cameraOn = document.querySelector('#c1'),
-	 cameraOff = document.querySelector('#c2'),
-	 downloadMap = document.querySelector('#download'),
-	 blocPop = document.querySelector('#bloc-pop');
+    var drag = new nsViewer.Drag();
+    drag.dragable('fullmap', 'fullmap');
 
-	var elemements = {
-		'elementBattery' : elementBattery,
-		'elementPosition' : elementPosition,
-		'elementMaterial' : elementMaterial,
-		'elementMove' : elementMove,
-		'elementMiniMap' : elementMiniMap,
-		'elementControl' : elementControl,
-		control : {
-			'wireframeOn' : wireframeOn,
-			'wireframeOff' : wireframeOff,
-			'moreFog' : moreFog,
-			'lessFog' : lessFog,
-			'cameraOn' : cameraOn,
-			'cameraOff' : cameraOff,
-			'downloadMap' : downloadMap,
-			'terrain' : [terrain]
-			},
-		pop : {'blocPop' : blocPop, 'classPop' : 'pop'}
-	}
-	
-	var elemementViewer = viewer.viewers[renderDiv],
-	  interfaces = new nsViewer.Interface(elemements, elemementViewer);
+    document.querySelector('#bloc-panel-minimap-hover').onclick = function (){
+	document.querySelector('#bloc-over-full').className = 'show';
+    }
+    document.querySelector('#bloc-fullMap .glyphicon-close').onclick = function (){
+	document.querySelector('#bloc-over-full').className = 'hidden';
+    }
 
-	var drag = new nsViewer.Drag();
-	drag.dragable('fullmap', 'fullmap');
+    /* Change camera elevation. */
+    observable.subscribe('rover.scanElevation.end', function(data) {
+	viewer.viewers[renderDiv].camera.position.y += data.elevation;
+    });
 
-	document.querySelector('#bloc-panel-minimap-hover').onclick = function (){
-		document.querySelector('#bloc-over-full').className = 'show';
-	}
-	document.querySelector('#bloc-fullMap .glyphicon-close').onclick = function (){
-		document.querySelector('#bloc-over-full').className = 'hidden';
-	}
+    /* When rover spawn display all informations and move camera. */
+    observable.subscribe('rover.spawn', function(data) {
+	document.querySelector('#bloc-panel-minimap').style.display = 'block';
+	document.querySelector('#bloc-panel-minimap-hover').style.display = 'block';
+	document.querySelector('#panel-minimap').style.display = 'block';
+	document.querySelector('#panel-infos').style.display = 'block';
+	document.querySelector('#bloc-pop').style.display = 'block';
+	document.querySelector('.loader').style.display = 'none';
 
-	/* Listen to rover events. */
-	var observable = new nsCommon.Observable();
-
-	observable.subscribe('rover.scanMaterial.end', function(data) {
-
-	});
-	observable.subscribe('rover.scanElevation.end', function(data) {
-		// Change camera elevation
-		viewer.viewers[renderDiv].camera.position.y += data.elevation;
-	});
-	observable.subscribe('rover.spawn', function(data) {
-    document.querySelector('#bloc-panel-minimap').style.display = 'block';
-    document.querySelector('#bloc-panel-minimap-hover').style.display = 'block';
-    document.querySelector('#panel-minimap').style.display = 'block';
-    document.querySelector('#panel-infos').style.display = 'block';
-    document.querySelector('#bloc-pop').style.display = 'block';
-    document.querySelector('.loader').style.display = 'none';
-
-    var ratio = viewer.viewers[renderDiv].MAP_RATIO,
+	/* Define camera position. */
+	var ratio = viewer.viewers[renderDiv].MAP_RATIO,
         init_x = (-ratio/2)*map.getWidth(),
         init_z = (-ratio/2)*map.getHeight();
-    viewer.viewers[renderDiv].camera.position.x = init_x + ratio*data.rover.x;
-    viewer.viewers[renderDiv].camera.position.z = init_z + ratio*data.rover.y;
-	});
-	observable.subscribe('rover.actions.fillTank.end', function(data) {
 
-	});
-	observable.subscribe('rover.actions.deploySolarPanels.end', function(data) {
+	viewer.viewers[renderDiv].camera.position.x = init_x + ratio*data.rover.x;
+	viewer.viewers[renderDiv].camera.position.z = init_z + ratio*data.rover.y;
+    });
 
-	});
-  observable.subscribe('rover.direction.end', function(data) {
+    /* Initialize rover mission related variables. */
+    var module,
+    startX,
+    startY,
+    endX,
+    endY;
 
-  });
-  observable.subscribe('rover.move.end', function(data) {
+    /* Get mission arguments. */
+    if(document.querySelector('#explorer').checked) {
+	module = 'explorer';
+	startX = parseInt(document.querySelector('#explorer-startX').value);
+	startY = parseInt(document.querySelector('#explorer-startY').value);
+    } else {
+	module = 'voyager';
+	startX = parseInt(document.querySelector('#voyager-startX').value);
+	startY = parseInt(document.querySelector('#voyager-startY').value);
+	endX = parseInt(document.querySelector('#voyager-endX').value);
+	endY = parseInt(document.querySelector('#voyager-endY').value);
+    }
 
-  });	
+    /* Define energy. */
+    var energy = parseInt(document.querySelector('#voyager-energy').value);
 
-	/* Rover tests. */
-  var module,
-      startX,
-      startY,
-      endX,
-      endY;
-  if(document.querySelector('#explorer').checked) {
-    module = 'explorer';
-    startX = parseInt(document.querySelector('#explorer-startX').value);
-    startY = parseInt(document.querySelector('#explorer-startY').value);
-  } else {
-    module = 'voyager';
-    startX = parseInt(document.querySelector('#voyager-startX').value);
-    startY = parseInt(document.querySelector('#voyager-startY').value);
-    endX = parseInt(document.querySelector('#voyager-endX').value);
-    endY = parseInt(document.querySelector('#voyager-endY').value);
-  }
-  var energy = parseInt(document.querySelector('#voyager-energy').value);
+    /* Instanciate rover at defined position on the map. */
+    var rover = new nsRover.Rover(map, startX, startY, energy);
 
-	var rover = new nsRover.Rover(map, startX, startY, energy);
-	var speculator = new nsSpeculator.S3000(rover);
+    /* Instanciate speculator and attach it to the rover. */
+    var speculator = new nsSpeculator.S3000(rover);
 
-	speculator.enableModule(module);
-	if(module == 'voyager'){
+    /* Enable the choosen module and start it with selected parameters. */
+    speculator.enableModule(module);
+
+    if (module == 'voyager') {
         speculator.start({x: endX, y: endY});
-	}
-	else {
-	    speculator.start();
-	}
-
-	console.log(rover.memory.readAll());
+    }
+    else {
+	speculator.start();
+    }
 })();
