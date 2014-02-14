ae97835c2cde349efee699cbe06a9d06a387a7db
Romain <r.esilva@sysdream.com>
2014-02-14 18:29:38 +0100

    Change indent of viewer to 4 spaces

--- assets/js/lib/Mars/viewer/viewer.js
+++ assets/js/lib/Mars/viewer/viewer.js
@@ -1,56 +1,57 @@
 (function() {
-	var nsViewer = using('mars.viewer');
+    var nsViewer = using('mars.viewer');
+    
 
-	/**
-	 * Constructor
-	 */
-	nsViewer.Viewer = function(map) {
-		this.map = map;
-		this.viewers = {};
-		this._allowedType = {
-			'2d': 'Viewer2D',
-			'3d': 'Viewer3D'
-		};
+    /**
+     * Constructor
+     */
+    nsViewer.Viewer = function(map) {
+	this.map = map;
+	this.viewers = {};
+	this._allowedType = {
+	    '2d': 'Viewer2D',
+	    '3d': 'Viewer3D'
 	};
+    };
 
-	/**
-	 * Check if the viewer is instanciated
-	 */
-	nsViewer.Viewer.prototype.hasViewer = function(element) {
-		if (this.viewers.hasOwnProperty(element)) {
-			return true;
-		}
+    /**
+     * Check if the viewer is instanciated
+     */
+    nsViewer.Viewer.prototype.hasViewer = function(element) {
+	if (this.viewers.hasOwnProperty(element)) {
+	    return true;
+	}
 
-		return false;
-	};
+	return false;
+    };
 
-	/**
-	 * Instancy a viewer if it's available
-	 */
-	nsViewer.Viewer.prototype.load = function(type, element, options, moved) {
-		// Check if the viewer typr is available
-		if (this._allowedType.hasOwnProperty(type)) {
-			type = this._allowedType[type];
-			// When the viewer is not already loaded
-			if (!this.hasViewer(element)) {
-				this.viewers[element] = new nsViewer[type](this, element, options, moved);
-				// Return new instance of viewer
-				return this.viewers[element];
-			}
-		}
-	};
+    /**
+     * Instancy a viewer if it's available
+     */
+    nsViewer.Viewer.prototype.load = function(type, element, options, moved) {
+	// Check if the viewer typr is available
+	if (this._allowedType.hasOwnProperty(type)) {
+	    type = this._allowedType[type];
+	    // When the viewer is not already loaded
+	    if (!this.hasViewer(element)) {
+		this.viewers[element] = new nsViewer[type](this, element, options, moved);
+		// Return new instance of viewer
+		return this.viewers[element];
+	    }
+	}
+    };
 
-	/**
-	 * Load 2D Viewer
-	 */
-	nsViewer.Viewer.prototype.load2D = function(element, options, moved) {
-		return this.load('2d', element, options, moved);
-	};
+    /**
+     * Load 2D Viewer
+     */
+    nsViewer.Viewer.prototype.load2D = function(element, options, moved) {
+	return this.load('2d', element, options, moved);
+    };
 
-	/**
-	 * Load 3D Viewer
-	 */
-	nsViewer.Viewer.prototype.load3D = function(element, options) {
-		return this.load('3d', element, options);
-	};
+    /**
+     * Load 3D Viewer
+     */
+    nsViewer.Viewer.prototype.load3D = function(element, options) {
+	return this.load('3d', element, options);
+    };
 })();
